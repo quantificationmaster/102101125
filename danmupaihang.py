89fc204247efc073d9faba94b832a5d41566b4dd
兴业狐狸大道 <97659380+quantificationmaster@users.noreply.github.com>
2023-09-14 20:33:06 +0800

    Add files via upload

--- danmupaihang.py
+++ danmupaihang.py
@@ -1,8 +1,19 @@
 from shlex import join
 from imageio import imread
 from collections import Counter
+import requests
+import re
+import json
+import csv
 import jieba
 import wordcloud
 f =open('danmu.csv', encoding='utf-8-sig')
 line =f.read().splitlines()
-print(Counter(line).most_common(20))
+
+counter = Counter(line)
+pai = counter.most_common(20)
+for value,count in pai:
+    print(value,count)
+
+
+
